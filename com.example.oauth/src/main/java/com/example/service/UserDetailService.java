b239afdac87e723def64f238ddf8a37694c7c931
iceameri <jwoo5264@gmail.com>
2024-03-19 14:27:10 +0900

    fix: authwired -> final로 변경

--- com.example.oauth/src/main/java/com/example/service/UserDetailService.java
+++ com.example.oauth/src/main/java/com/example/service/UserDetailService.java
@@ -2,16 +2,16 @@ package com.example.service;
 
 import com.example.dto.User;
 import com.example.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserDetailService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
