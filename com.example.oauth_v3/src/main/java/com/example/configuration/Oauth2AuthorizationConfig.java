fd798683095395173daf876abc0478ac87aa6a21
iceameri <jwoo5264@gmail.com>
2024-03-28 17:23:46 +0900

    fix: TokenEnhancer 추가

--- com.example.oauth_v3/src/main/java/com/example/configuration/Oauth2AuthorizationConfig.java
+++ com.example.oauth_v3/src/main/java/com/example/configuration/Oauth2AuthorizationConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -27,37 +27,35 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     private final AuthenticationManager authenticationManager;
     private final DataSource dataSource;
     @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
+    public TokenStore tokenStore() { return new JdbcTokenStore(dataSource); }
     @Bean
     public ApprovalStore approvalStore() { return new JdbcApprovalStore(dataSource); }
     @Bean
     public JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices() { return new JdbcAuthorizationCodeServices(dataSource); }
-
-    @Bean // 커스텀해서 사용할듯함
-    public JdbcClientDetailsService jdbcClientDetailsService() { return new JdbcClientDetailsService(dataSource); }
+    @Bean
+    public TokenEnhancer tokenEnhancer() { return new CustomTokenEnhancer(); }
+//    @Bean // 커스텀해서 사용할듯함 / endpoints에 추가못함
+//    public JdbcClientDetailsService jdbcClientDetailsService() { return new JdbcClientDetailsService(dataSource); }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) {
-        security.checkTokenAccess("permitAll()");
+        security.checkTokenAccess("permitAll()"); // 토큰유효성(/token/check_token) 접근을 위해 설정
     }
 
-    // client 설정
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource);
     }
 
-    // 인증, 토큰 설정
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailService) // refresh token 발행시 유저 정보 검사 하는데 사용하는 서비스 설정
-                .tokenStore(tokenStore())
+                .tokenStore(tokenStore()) // framework가 자동으로 token 저장
                 .approvalStore(approvalStore())
                 .authorizationCodeServices(jdbcAuthorizationCodeServices())
+                .tokenEnhancer(tokenEnhancer()) // 토큰 발급시 추가정보 입력
         ;
     }
 }
