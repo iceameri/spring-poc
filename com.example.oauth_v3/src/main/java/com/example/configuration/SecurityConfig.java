3f9ef0c91c2388d36c82bcedba7fc75490246c96
iceameri <jwoo5264@gmail.com>
2024-04-15 10:21:20 +0900

    fix: 로그인되지않는 현상 해결

--- com.example.oauth_v3/src/main/java/com/example/configuration/SecurityConfig.java
+++ com.example.oauth_v3/src/main/java/com/example/configuration/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.example.configuration;
 
 import com.example.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity // 웹보안 활성화
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -21,15 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic(); // http 통신으로 basic auth를 사용 할 수 있다. (ex: Authorization: Basic bzFbdGfmZrptWY30YQ==)
     }
 
-    @Primary
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean(); // authenticationManager bean 생성 하여 셋팅 안할시 grant_type : password 지원 안함
     }
 
-    @Bean
-    public CustomUserDetailsService userDetailService(){
-        return new CustomUserDetailsService();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService);
     }
+
 }
