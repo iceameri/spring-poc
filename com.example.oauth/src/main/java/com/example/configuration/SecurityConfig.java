6777a383794f0f1b45341ca1f05dd762fa5aec89
iceameri <jwoo5264@gmail.com>
2024-04-15 10:20:24 +0900

    chore

--- com.example.oauth/src/main/java/com/example/configuration/SecurityConfig.java
+++ com.example.oauth/src/main/java/com/example/configuration/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.configuration;
 
 import com.example.service.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable() // csrf 공격을 막기 위해 state 값을 전달 받지 않는다
+        http
+                .csrf().disable() // csrf 공격을 막기 위해 state 값을 전달 받지 않는다
                 .formLogin() // 기본 제공하는 로그인 화면 사용
                 .and()
                 .httpBasic(); // http 통신으로 basic auth를 사용 할 수 있다. (ex: Authorization: Basic bzFbdGfmZrptWY30YQ==)
